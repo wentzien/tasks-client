25b160de56963c568c3088e19b7a31aac3f68621
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-19 07:44:42 +0200

    update routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    Switch,
+    Route,
+    Redirect,
+    BrowserRouter as Router
+} from "react-router-dom";
+
+import "./styles/normalize.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/globals.css";
+
+import NavBar from "./components/general/NavBar";
+
+import Login from "./pages/login";
+import Logout from "./pages/logout";
+import Register from "./pages/register";
+import Tasklist from "./pages/tasklist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavBar/>
+            <Switch>
+                <Route exact path="/" component={Tasklist}></Route>
+                <Route exact path="/tasklists" component={Tasklist}></Route>
+                <Route exact path="/accounts/login" component={Login}></Route>
+                <Route exact path="/accounts/logout" component={Logout}></Route>
+                <Route exact path="/accounts/register" component={Register}></Route>
+                <Route exact path="/*">
+                    <Redirect to="/404"/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
