4875963c6b6b6e280c5302e1bdfff5b5a56365d9
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-23 23:01:36 +0200

    update registerform to new auth context api

--- src/components/authentication/RegisterForm.jsx
+++ src/components/authentication/RegisterForm.jsx
@@ -1,56 +1,121 @@
 import React from "react";
-import FormComponent from "../forms/Form";
-import Joi from "joi-browser";
-import Form from "react-bootstrap/Form";
-import userService from "../../services/userService";
-import authService from "../../services/authService";
+import * as yup from "yup";
+import {Formik, Form, useField} from "formik";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import useAuth from "../../hooks/useAuth";
 
-class RegisterForm extends FormComponent {
-    state = {
-        data: {
-            name: "",
-            email: "",
-            password: ""
-        },
-        errors: {}
-    };
+const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+};
 
-    schema = {
-        name: Joi.string().min(3).max(255).required().label("Name"),
-        email: Joi.string().email().max(255).required().label("E-Mail"),
-        password: Joi.string().min(5).max(255).required().label("Password")
-    }
+const validationSchema = yup.object({
+    name: yup
+        .string("Enter your name")
+        .required("Name is required"),
+    email: yup
+        .string("Enter your email")
+        .email("Enter a valid email")
+        .required("Email is required"),
+    password: yup
+        .string("Enter your password")
+        .min(5, "Password should be of minimum 5 characters length")
+        .required("Password is required"),
+});
 
-    submitData = async () => {
-        try {
-            const response = await userService.register(this.state.data);
-            authService.loginWithJwt(response.headers["x-auth-token"]);
-            window.location = "/";
-        } catch (ex) {
-            if (ex.response && ex.response.status === 400) {
-                const errors = {...this.state.errors};
-                errors.email = ex.response.data;
-                this.setState({errors});
-            }
-        }
-    };
+const FormikTextField = ({...props}) => {
+    const [field, meta] = useField(props);
 
-    render() {
-        if(authService.getCurrentUser()){
-            const {router} = this.props;
-            router.push("/");
-        }
-
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <h1>Register</h1>
-                {this.renderInputField("name", "Name")}
-                {this.renderInputField("email", "E-Mail", "email")}
-                {this.renderInputField("password", "Password", "password")}
-                {this.renderSubmitButton("Register")}
-            </Form>
-        );
-    }
+    return (
+        <>
+            <TextField
+                {...field}
+                {...props}
+                error={Boolean(meta.touched && meta.error)}
+                helperText={meta.touched && meta.error}
+            />
+        </>
+    );
 }
+
+const RegisterForm = () => {
+    const {register} = useAuth();
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={async (values, {setStatus, setSubmitting}) => {
+                try {
+                    await register(values);
+
+                } catch (ex) {
+                    if (ex.response && ex.response.status === 400) {
+                        setStatus(ex.response.data);
+                        setSubmitting(false);
+                    }
+                }
+            }}
+        >
+            {({
+                  status,
+                  isSubmitting,
+                  isValid,
+                  dirty
+              }) => (
+                <Form>
+                    <FormikTextField
+                        label="Name"
+                        name="name"
+                        type="text"
+                        autoFocus
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <FormikTextField
+                        label="Email Address"
+                        name="email"
+                        type="email"
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <FormikTextField
+                        label="Password"
+                        name="password"
+                        type="password"
+                        fullWidth
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    {status && (
+                        <Box sx={{mt: 3}}>
+                            <FormHelperText error>
+                                {status}
+                            </FormHelperText>
+                        </Box>
+                    )}
+                    <Box sx={{mt: 2}}>
+                        <Button
+                            color="primary"
+                            disabled={isSubmitting || !isValid || !dirty}
+                            fullWidth
+                            size="large"
+                            type="submit"
+                            variant="contained"
+                        >
+                            Register
+                        </Button>
+                    </Box>
+                </Form>
+            )}
+        </Formik>
+    );
+};
 
 export default RegisterForm;
