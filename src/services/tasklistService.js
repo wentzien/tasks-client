58039ca0b9f6cac8a65cb9602f703370ed1cc7d9
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-24 10:22:59 +0200

    shift of error logic to components

--- src/services/tasklistService.js
+++ src/services/tasklistService.js
@@ -2,43 +2,23 @@ import {apiUrl} from "../config.json";
 import http from "./httpService";
 
 async function getAll() {
-    try {
-        const response = await http.get(apiUrl + "/tasklists");
-        return response.data;
-
-    } catch (ex) {
-        console.log(ex);
-    }
+    const response = await http.get(apiUrl + "/tasklists");
+    return response.data;
 }
 
 async function getById(tasklistId) {
-    try {
-        const response = await http.get(apiUrl + "/tasklists/" + tasklistId);
-        return response.data;
-
-    } catch (ex) {
-        console.log(ex);
-    }
+    const response = await http.get(apiUrl + "/tasklists/" + tasklistId);
+    return response.data;
 }
 
 async function create(data) {
-    try {
-        const response = await http.post(apiUrl + "/tasklists", data);
-        return response.data;
-
-    } catch (ex) {
-        console.log(ex);
-    }
+    const response = await http.post(apiUrl + "/tasklists", data);
+    return response.data;
 }
 
 async function remove(tasklistId) {
-    try {
-        const response = await http.post(apiUrl + "/tasklists/" + tasklistId);
-        return response.data;
-
-    } catch (ex) {
-        console.log(ex);
-    }
+    const response = await http.post(apiUrl + "/tasklists/" + tasklistId);
+    return response.data;
 }
 
 export default {
