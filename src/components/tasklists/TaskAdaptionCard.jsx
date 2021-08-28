9d82c0eca01065413506d37a9c3bfc061b3d73b8
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-08-28 20:00:15 +0200

    make task adaption card sticky to the top

--- src/components/tasklists/TaskAdaptionCard.jsx
+++ src/components/tasklists/TaskAdaptionCard.jsx
@@ -4,7 +4,7 @@ import Card from "@material-ui/core/Card";
 
 const TaskAdaptionCard = ({task, onSubmit, onMarkImportant, onDelete, onClose}) => {
     return (
-        <Card>
+        <Card sx={{position: "sticky", top: 10}}>
             <CardContent sx={{pt: 1}}>
                 <TaskAdaptionForm
                     task={task}
